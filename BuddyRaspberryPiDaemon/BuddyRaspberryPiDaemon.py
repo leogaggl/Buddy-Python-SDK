2f896aea69b7027701d807b768b1dc5082865f94
Brad Serbus <brad@buddy.com>
2016-01-17 12:09:51 -0800

    Finish GPIO daemon.

--- BuddyRaspberryPiDaemon/BuddyRaspberryPiDaemon.py
+++ BuddyRaspberryPiDaemon/BuddyRaspberryPiDaemon.py
@@ -1,33 +1,42 @@
-﻿import RPIO
-import select
-import sys
+﻿import sys
+import time
+from curses_check_for_keypress import CheckForKeypress
+#import RPi.GPIO as GPIO
+from RPi import GPIO
+GPIO.VERBOSE = False
 
-import buddy
 
+GPIO.setmode(GPIO.BOARD)
 
-print('Buddy Raspberry Pi SDK Sample')
+channel_list = [11, 12]
+
+GPIO.setup(channel_list, GPIO.IN)
+
+sys.path.append("../Buddy Python SDK")
+sys.path.append("../Buddy Python SDK/buddysdk")
+
+from buddysdk import buddy
 
 buddy.init("bbbbbc.xgjbvPdwkllw", "1E9E824E-A3F1-4F34-B4F4-9CC87471A564")
 
 # A PUT must be done to configure your app's telemetry. See https://dev.buddyplatform.com/docs/IoT%20Telemetry#ConfigureTelemetry for more details.
 buddy.put("/telemetry/RaspberryPi", {})
 
+c = CheckForKeypress('Buddy Raspberry Pi SDK Sample', test_mode=True)
 
+while c.input() is None:
 
-while True:
+    outputs = [GPIO.input(channel) for channel in channel_list]
 
-	i, o, e = select.select( [sys.stdin], [], [], 5 )
+    data = {"pins": dict(zip(channel_list, outputs))}
 
+    response = buddy.post("/telemetry/RaspberryPi", {"data": data})
 
-	options = {
-		"data" : { "value_a" : 1, "value_b" : True },
-		"location" : "47.1, -121.292"
-	}
+    print("Telemetry response: " + str(response))
 
+    time.sleep(2)
 
-	buddy.post("/telemetry/RaspberryPi", options)
+GPIO.cleanup(channel_list)
 
-
-	if (i):
-		break
+c.cleanup()
 
